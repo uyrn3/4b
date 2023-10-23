49cda86c37b2329cf65784628f292c847e3b1744
uyrn3 <emeira127@gmail.com>
2023-10-23 07:53:33 -0300

    23/10

---
+++ Lista/conceito.py
@@ -0,0 +1,23 @@
+# lista = [1, 0.5, 'PYTHON'] # - criar a lista
+# print(lista)
+
+# lista = [10,20,30,40]
+
+# Exibir os valores/elementos da lista
+# for numeros in lista:
+#    print(numeros)
+# indice 0, 1, 2, 3
+# lista = [10,20,30,40]
+# print(lista)
+# lista.append(int(input("Informe um inteiro: ")))# Acresentar ao final da lista
+# numero = 2.5
+# lista.insert(2,numero)#(indice,valor) # Inseir o valor na posição desejada
+# print(lista)
+
+# Criar a lista com quantidade de valores definida pelo usuario
+quant = int(input("Informe a quant de numeros da lista: "))
+lista_numeros = []
+for i in range(quant):
+    numeros = int(input("Informe: "))
+    lista_numeros.append(numeros) # Salvar os valores na lista
+print(lista_numeros)
